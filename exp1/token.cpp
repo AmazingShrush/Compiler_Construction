b95a0d090a53f32cdacfd67a2aa7783a979b21fb
Shrusti <16co52@aitdgoa.edu.in>
2019-07-23 14:41:20 +0530

    Changed again

--- exp1/token.cpp
+++ exp1/token.cpp
@@ -8,30 +8,58 @@ int main(){
 		int a=n.length();
 		for(i=0;i<a;i++){
 			if(n[i]=='f'&&n[i+1]=='o'&&n[i+2]=='r'){
-				cout<<"for: keyword"<<endl;
+				cout<<"\tfor is a keyword"<<endl;
 				i=i+2;
 			}
 			else if(n[i]=='w' && n[i+1]=='h' && n[i+2]=='i' && n[i+3]=='l' && n[i+4]=='e'){
-					cout<<"while : keyword"<<endl;
+					cout<<"\twhile is a keyword"<<endl;
 					i=i+4;
 			
 			}
 			else if(n[i]=='d' && n[i+1]=='o'){
-				cout<<"do is a keyword"<<endl;
+				cout<<"\tdo is a keyword"<<endl;
 				i=i+1;
+			}
+			else if(n[i]=='i' && n[i+1]=='n' && n[i+2]=='t'){
+				cout<<"\tint is a keyword"<<endl;
+				i=i+2;
+			}
+			else if(n[i]=='s' && n[i+1]=='i' && n[i+2]){
+				cout<<"\tsin is a keyword"<<endl;
+				i=i+2;
+			}
+			else if(n[i]=='i' && n[i+1]=='f'){
+				cout<<"\tif is a keyword"<<endl;
+				i=i+1;
+			}
+			else if(n[i]=='e' && n[i+1]=='l' && n[i+2]=='s' && n[i+3]=='e'){
+				cout<<"\telse is a keyword"<<endl;
+				i=i+3;
 			}
 			else if(n[i]>=48 && n[i]<=57){
 				
-			cout<<n[i]<<" is a constant"<<endl;
+			cout<<"\t"<<n[i]<<" is a constant"<<endl;
 			}
-			else if(n[i]==40 || n[i]==41 || n[i]==';' || n[i]==123 || n[i]==125 || n[i]==91 || n[i]==93){
-				cout<<n[i]<<" is a delimeter "<<endl;
+			else if(n[i]==40 || n[i]==41 || n[i]==';' || n[i]==123 || n[i]==125 || n[i]==91 || n[i]==93 ||n[i]==','){
+				cout<<"\t"<<n[i]<<" is a delimeter "<<endl;
+			}
+			else if(n[i]=='+' && n[i+1]=='+'){
+				cout<<"\t++ is an operator"<<endl;
+				i=i+1;
+			}
+			else if(n[i]=='-' && n[i+1]=='-'){
+				cout<<"\t-- is an operator"<<endl;
+				i=i+1;
+			}
+			else if(n[i]=='=' && n[i+1]=='='){
+				cout<<"\t== is an operator"<<endl;
+				i=i+1;
 			}
 			else if(n[i]=='+' || n[i]=='-' || n[i]=='*' || n[i]=='/' || n[i]=='%' || n[i]=='=' || n[i]=='>' || n[i]=='>' || n[i]=='='){
-				cout<<n[i]<<" is an operator"<<endl;
+				cout<<"\t"<<n[i]<<" is an operator"<<endl;
 			}
 			else{
-				cout<<n[i]<<" is an identifier "<<endl;
+				cout<<"\t"<<n[i]<<" is an identifier "<<endl;
 			}
 		
 	}
